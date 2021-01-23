199b149c32d2b390ae51cc262c938b6f8b8ff7f6
MiPc <MiPc@192.168.1.140>
2021-01-23 11:33:39 +0100

    Merge origin/main
    Conflicts:
    	README.md

---
+++ UncagedData/public_html/js/plot1.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+//amb node:
+const d3 = require("d3");
+
+var paragraphs = document.getElementsByTagName("p");
+
+for (var i = 0; i < paragraphs.length; i++) {
+  var paragraph = paragraphs.item(i);
+  paragraph.style.setProperty("color", "blue", null);
+}
+
+var doc = document.getElementById('region');
+doc.innerHTML = "hey ho lets go!";
