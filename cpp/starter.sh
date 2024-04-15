f610cdd048f449dc3d1391413589da5130d4fde9
roma <romdommas@gmail.com>
2024-04-15 14:26:26 +0200

    added a lot of files regarding the inversions en borsa analysis that are needed on my shared server

--- cpp/starter.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo Barxelona_11 | sudo -S /var/www/escolamatem/cpp/dailies > /var/www/escolamatem/cpp/salida.log 2>&1
---
+++ cpp/starter_main_stocks.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo Barxelona_11 | sudo -S /var/www/escolamatem/cpp/anal_main > /var/www/escolamatem/cpp/sortida_main.log 2>&1
