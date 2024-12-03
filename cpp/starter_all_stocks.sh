0a3f3ee0e151df790dad704cff99127f8d578605
roma <romdommas@gmail.com>
2024-12-03 01:40:28 +0100

    a

--- cpp/starter_all_stocks.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo barxelona | sudo -S /var/www/escolamatem/cpp/anal_tot > /var/www/escolamatem/cpp/output_anal_tot.log 2>&1
--- cpp/starter_main_stocks.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo Barxelona_11 | sudo -S /var/www/escolamatem/cpp/anal_main > /var/www/escolamatem/cpp/sortida_main.log 2>&1
